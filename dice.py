9b3a0f6f2d0764a31a20112a888283b57f290edc
Ellas25 <97697077+Ellas25@users.noreply.github.com>
2022-04-08 12:57:55 +0100

    Update dice.py

--- dice.py
+++ dice.py
@@ -1,6 +1,7 @@
 import pygame
 import boardgame
-
+import random 
+"""
 def dice(d):
     if d == 1:
         d = constant.DICE1_IMAGE_NAME
@@ -13,9 +14,18 @@ def dice(d):
     elif d == 5:
         d = constant.DICE5_IMAGE_NAME
     elif d == 6:
-        d = constant.DICE6_IMAGE_NAME
+        d = constant.DICE6_IMAGE_NAME This was apparently plagarised 
+"""
+def dice(di):
+    minimum_value = 1
+    maximum_value = 7
+    rolls_dice = "Yes" or "yes" or "y" or "Y"
+    
+    while rolls_dice = "Yes" or "yes" or "y" or "Y":
+        print random.randint (minimum_value,maximum_value)
+        print random.randint( minimum_value,emaximum_value )
 
-    time_clock = pygame.time.get_ticks()
-    while pygame.time.get_ticks() - time_clock < 1000:
+    clock_times = pygame.time.get_ticks()
+    while pygame.time.get_ticks() - clock_time < 3000:
         set_up.game_layout_display.blit(d, (300, 500))
         pygame.display.update()
