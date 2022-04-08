f4b2b5f2a490dbd32b410b68de4f508ef3e59187
Ellas25 <97697077+Ellas25@users.noreply.github.com>
2022-04-08 13:15:14 +0100

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -3,7 +3,55 @@ import set_up
 import message_displays
 import quit
 
-def centre_button(button_name, xm, ym, x, y, wid, hei, initial_color, after_color, size, type):
+class centre_button()
+#button_name, xm, ym, x, y, wid, hei, initial_color, after_color, size, type):
+def __init__( xm, ym, x, y, wid, hei, initial_color, after_color, size, type, bg="blue"):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Comic Sans", font)
+        if buttons == "":
+            self. = "text"
+        else:
+           self.buttons = feedback
+        self.change_text(text, bg)
+        def change_text(self, text, bg="black"):
+        
+        self.text = self.font.render(text, 1, set_up.pygame.Color())
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Oval(self.x, self.y, self.size[0], self.size[1])
+ 
+    def butts(self):
+        screen.blit(button1.surface, (self.x, self.y))
+ 
+    def clicks_on_button(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
+ 
+ 
+def mainloop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            button1.click(event)
+        button1.show()
+        clock.tick(30)
+        pygame.display.update()
+ 
+ 
+button1 = Button(
+    "Click here",
+    (100, 100),
+    font=30,
+    bg="navy",
+    feedback="You clicked me")
+  
+"""
     if x + wid > xm > x and y + hei > ym > y:
         pygame.draw.rect(set_up.game_layout_display, after_color, [x - 2.5, y - 2.5, wid + 5, hei + 5])
         if pygame.mouse.get_pressed() == (1, 0, 0):
@@ -43,5 +91,103 @@ def play_button(t, xm, ym, x, y, wid, hei, int, after, fast):
             return True
 
     else:
+    
+ 
         pygame.draw.rect(set_up.game_layout_display, int, [x, y, wid, hei])
     message_displays.message_display_screen(t, (x + wid + x) / 2, (y + hei + y) / 2, fast)
+"""
+class buttons_side():
+#button_name, xm, ym, x, y, wid, hei, initial_color, after_color, size, type):
+def __init__( xm, ym, x, y, wid, hei, initial_color, after_color, size, type, bg="blue"):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Comic Sans", font)
+        if buttons == "":
+            self. = "text"
+        else:
+           self.buttons = feedback
+        self.change_text(text, bg)
+        def change_text(self, text, bg="black"):
+        
+        self.text = self.font.render(text, 1, set_up.pygame.Color())
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Oval(self.x, self.y, self.size[0], self.size[1])
+ 
+    def butts(self):
+        screen.blit(button1.surface, (self.x, self.y))
+ 
+    def clicks_on_button(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
+ 
+ 
+def mainloop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            button1.click(event)
+        button1.show()
+        clock.tick(30)
+        pygame.display.update()
+ 
+ 
+button1 = Button(
+    "Click here",
+    (100, 100),
+    font=30,
+    bg="navy",
+    feedback="You clicked me")
+
+class play_the_button()
+#button_name, xm, ym, x, y, wid, hei, initial_color, after_color, size, type):
+def __init__( xm, ym, x, y, wid, hei, initial_color, after_color, size, type, bg="blue"):
+        self.x, self.y = pos
+        self.font = pygame.font.SysFont("Comic Sans", font)
+        if buttons == "":
+            self. = "text"
+        else:
+           self.buttons = feedback
+        self.change_text(text, bg)
+        def change_text(self, text, bg="black"):
+        
+        self.text = self.font.render(text, 1, set_up.pygame.Color())
+        self.size = self.text.get_size()
+        self.surface = pygame.Surface(self.size)
+        self.surface.fill(bg)
+        self.surface.blit(self.text, (0, 0))
+        self.rect = pygame.Oval(self.x, self.y, self.size[0], self.size[1])
+ 
+    def butts(self):
+        screen.blit(button1.surface, (self.x, self.y))
+ 
+    def clicks_on_button(self, event):
+        x, y = pygame.mouse.get_pos()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if pygame.mouse.get_pressed()[0]:
+                if self.rect.collidepoint(x, y):
+                    self.change_text(self.feedback, bg="red")
+ 
+ 
+def mainloop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            button1.click(event)
+        button1.show()
+        clock.tick(30)
+        pygame.display.update()
+ 
+ 
+button1 = Button(
+    "Click here",
+    (100, 100),
+    font=30,
+    bg="navy",
+    feedback="You clicked me")
