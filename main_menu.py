b292f245acda1bcfedd3c0394a5e99eca5fc4c4a
Ellas25 <97697077+Ellas25@users.noreply.github.com>
2022-04-15 11:42:04 +0100

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -26,8 +26,8 @@ def main_menu():
             mouse = pygame.mouse.get_pos()
 
             set_up.game_layout_display.blit(set_up.menu_background, (0, 0))
-            player_button("Play", mouse[0], mouse[1], (constant.WIDTH / 2 - 100), constant.HEIGHT / 2, 200, 100, constant.green_color,
-                constant.blue_color, 60, 1)
+            player_button("Play", mouse[0], mouse[1], (constant.WIDTH / 2 - 100), constant.HEIGHT / 2, 200, 100, constant.purple_color,
+                constant.green_color, 60, 1)
 
             player_button("Quit", mouse[0], mouse[1], (constant.WIDTH / 2 - 100), (constant.HEIGHT / 2) + 200, 200, 100,
                 constant.red_color, constant.grey_color, 60, 0)
@@ -35,7 +35,7 @@ def main_menu():
             mouse = pygame.mouse.get_pos()
             if buttons.side_button("Mute Music", mouse[0], mouse[1], 1166, 0, 200, 50, constant.red_color, constant.purple_color, 25):
                 pygame.mixer.music.pause()
-            if buttons.side_button("Play Music", mouse[0], mouse[1], 1166, 75, 200, 50, constant.purple_color, constant.blue_color, 25):
+            if buttons.side_button("Play Music", mouse[0], mouse[1], 1166, 75, 200, 50, constant.purple_color, constant.grey, 25):
                 pygame.mixer.music.unpause()
             if buttons.side_button("Credits", mouse[0], mouse[1], 1166, 150, 200, 50, constant.grey_color, constant.red_color, 25):
                 creditation()
@@ -58,7 +58,7 @@ def creditation():
         # mouse position
         mouse = pygame.mouse.get_pos()
         click = pygame.mouse.get_pressed()
-        if buttons.centre_button("Back", mouse[0], mouse[1], constant.WIDTH / 2 - 100, 700, 200, 50, constant.red_color, constant.white_color, 25, 20):
+        if buttons.centre_button("Back", mouse[0], mouse[1], constant.WIDTH / 2 - 100, 700, 200, 50, constant.grey_color, constant.white_color, 25, 20):
             main_menu()
 
         pygame.display.update()
