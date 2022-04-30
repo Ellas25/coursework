4393e67f531b2b7b36d6a97405f80e9a0965a1ad
Ellas25 <97697077+Ellas25@users.noreply.github.com>
2022-04-30 11:39:53 +0100

    added colours

--- constant.py
+++ constant.py
@@ -1,43 +1,44 @@
+Constant.py 
 HEIGHT = 768
 WIDTH = 1366
-MOTHERBOARD_IMAGE_NAME = "src/resources/snakes.jpg"
+MOTHERBOARD_IMAGE_NAME = "resources/snakes.jpg"
 
-ICON = "src/resources/icon.jpg"
+ICON = "resources/icon.jpg"
 CAPTIONS = "snakes_and_ladders"
 
-DICE1_IMAGE_NAME = "src/resources/dice1.png"
-DICE2_IMAGE_NAME = "src/resources/dice2.png"
-DICE3_IMAGE_NAME = "src/resources/dice3.png"
-DICE4_IMAGE_NAME = "src/resources/dice4.png"
-DICE5_IMAGE_NAME = "src/resources/dice5.png"
-DICE6_IMAGE_NAME = "src/resources/dice6.png"
+DICE1_IMAGE_NAME = "resources/dice1.png"
+DICE2_IMAGE_NAME = "resources/dice2.png"
+DICE3_IMAGE_NAME = "resources/dice3.png"
+DICE4_IMAGE_NAME = "resources/dice4.png"
+DICE5_IMAGE_NAME = "resources/dice5.png"
+DICE6_IMAGE_NAME = "resources/dice6.png"
 
-MENU_BACKGROUND = "src/resources/game_background.jpg"
-SNAKE = "src/resources/snakes.jpg"
-OWNER = "src/resources/credits.png"
+MENU_BACKGROUND = "resources/game_background.jpg"
+SNAKE = "resources/snakes.jpg"
+OWNER = "resources/credits.png"
 
-COUNTER_TEAL_IMAGE_NAME = "src/resources/teal_c.png"
-COUNTER_GREY_IMAGE_NAME = "src/resources/grey_c.png"
-COUNTER_WHITE_IMAGE_NAME = "src/resources/white_c.png"
-COUNTER_PURPLE_IMAGE_NAME = "src/resources/purple_c.png"
+COUNTER_TEAL_IMAGE_NAME = "resources/teal_c.png"
+COUNTER_GREY_IMAGE_NAME = "resources/grey_c.png"
+COUNTER_WHITE_IMAGE_NAME = "resources/white_c.png"
+COUNTER_PURPLE_IMAGE_NAME = "resources/purple_c.png"
 
-INITIAL_BACKGROUND_IMAGE_NAME = "src/introduction_image.png"
-INITIAL_BACKGROUND_IMAGE1_NAME = "src/introduction_image1.png"
-INITIAL_BACKGROUND2_IMAGE_NAME = "src/introduction_image2.png"
-INITIAL_BACKGROUND3_IMAGE_NAME = "src/introduction_image3.png"
-INITIAL_BACKGROUND4_IMAGE_NAME = "src/introduction_image4.png"
-INITIAL_BACKGROUND5_IMAGE_NAME = "src/introduction_image5.png"
+INITIAL_BACKGROUND_IMAGE_NAME = "resources/introduction_image.png"
+INITIAL_BACKGROUND1_IMAGE_NAME = "resources/introduction_image1.png"
+INITIAL_BACKGROUND2_IMAGE_NAME = "resources/introduction_image2.jpg"
+INITIAL_BACKGROUND3_IMAGE_NAME = "resources/introduction_image3.jpg"
+INITIAL_BACKGROUND4_IMAGE_NAME = "resources/introduction_image4.jpg"
+INITIAL_BACKGROUND5_IMAGE_NAME = "resources/introduction_image5.jpg"
 
-PYGAME_MIXER_SOUND = "src/sound/musics.wav"
-SNAKE_SOUND = "src/sound/snake.wav"
-WIN_SOUND = "src/sound/win.wav"
-LOSE_SOUND = "src/sound/lose.wav"
-LADDER_SOUND = "src/sound/ladder.wav"
+PYGAME_MIXER_SOUND = "sound/music.wav"
+SNAKE_SOUND = "sound/snake.wav"
+WIN_SOUND = "sound/win.wav"
+LOSE_SOUND = "sound/lose.wav"
+LADDER_SOUND = "sound/ladder.wav"
 
 black_color = (20, 20, 20)
 white_color = (250, 250, 250)
 red_color = (200, 0, 0)
-blue_red_color = (260, 0, 0)
+blue_red_color = (250, 0, 0)
 green_color = (0, 200, 0)
 blue_green_color = (0, 230, 0)
 blue_color = (0, 0, 200)
@@ -46,7 +47,17 @@ yellow_color = (150, 150, 0)
 purple_color = (43, 3, 132)
 blue_purple_color = (60, 0, 190)
 
-CREDITS = "src/resources/owner.jpg"
-POSTS = "src/game_background.png"
+blue_l = (64, 74, 233)
+yellow_L = (255, 255, 0)
+blue_T = (4, 74, 63)
+red = (255, 0, 0)
+grey = (192, 192, 192)
+black = (0, 0, 0)
+green = (0, 128, 0)
+teal = (0, 128, 128)
+lime = (0, 255, 0)
 
-BUTTON_FONTS = "src/fonts/Qarolina.ttf"
+CREDITS = "resources/owner.jpg"
+POSTS = "resources/game_background.png"
+
+BUTTON_FONTS = "fonts/Qarolina.ttf"
